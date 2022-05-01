ea36e7fa7eb21812b7a7753a5559dee269ee5d46
Kevims <kcodeyt@gmail.com>
2022-05-01 14:51:12 +0200

    1.0.1: Implement form handlers and add properties to pom.xml (#3)
    * Add properties in pom.xml
    
    * Use new form handlers instead of event

--- src/main/java/ms/kevi/plotplugin/command/defaults/TeleportCommand.java
+++ src/main/java/ms/kevi/plotplugin/command/defaults/TeleportCommand.java
@@ -17,12 +17,8 @@
 package ms.kevi.plotplugin.command.defaults;
 
 import cn.nukkit.Player;
-import cn.nukkit.event.EventHandler;
-import cn.nukkit.event.Listener;
-import cn.nukkit.event.player.PlayerFormRespondedEvent;
 import cn.nukkit.form.element.ElementButton;
-import cn.nukkit.form.response.FormResponse;
-import cn.nukkit.form.response.FormResponseSimple;
+import cn.nukkit.form.handler.FormResponseHandler;
 import cn.nukkit.form.window.FormWindowSimple;
 import ms.kevi.plotplugin.PlotPlugin;
 import ms.kevi.plotplugin.command.PlotCommand;
@@ -30,9 +26,7 @@ import ms.kevi.plotplugin.command.SubCommand;
 import ms.kevi.plotplugin.lang.TranslationKey;
 import ms.kevi.plotplugin.manager.PlotManager;
 
-import java.util.HashMap;
 import java.util.Map;
-import java.util.function.Consumer;
 
 /**
  * @author Kevims KCodeYT
@@ -40,47 +34,30 @@ import java.util.function.Consumer;
  */
 public class TeleportCommand extends SubCommand {
 
-    private final Map<Player, Map<Integer, Consumer<FormResponse>>> map;
-
     public TeleportCommand(PlotPlugin plugin, PlotCommand parent) {
         super(plugin, parent, "teleport", "tp");
         this.setPermission("plot.command.admin.teleport");
-
-        this.map = new HashMap<>();
-
-        this.plugin.getServer().getPluginManager().registerEvents(new Listener() {
-            @EventHandler
-            public void onResponded(PlayerFormRespondedEvent event) {
-                final Map<Integer, Consumer<FormResponse>> map;
-                if((map = TeleportCommand.this.map.get(event.getPlayer())) != null) {
-                    final Consumer<FormResponse> consumer = map.get(event.getFormID());
-                    if(consumer != null) consumer.accept(event.getResponse());
-                }
-            }
-        }, this.plugin);
     }
 
     @Override
     public boolean execute(Player player, String[] args) {
-        Map<Integer, Consumer<FormResponse>> map;
-        if((map = this.map.get(player)) == null)
-            this.map.put(player, map = new HashMap<>());
-
         final Map<String, PlotManager> plotManagers = this.plugin.getPlotManagerMap();
         final FormWindowSimple window = new FormWindowSimple(this.translate(player, TranslationKey.TELEPORT_FORM_TITLE), "");
 
         for(String levelName : plotManagers.keySet())
             window.addButton(new ElementButton(levelName));
 
-        map.put(player.showFormWindow(window), response -> {
-            if(response instanceof FormResponseSimple) {
-                final PlotManager plotManager = plotManagers.get(((FormResponseSimple) response).getClickedButton().getText());
+        window.addHandler(FormResponseHandler.withoutPlayer(ignored -> {
+            if(!window.wasClosed()) {
+                final PlotManager plotManager = plotManagers.get(window.getResponse().getClickedButton().getText());
                 if(plotManager == null) return;
 
                 player.teleport(plotManager.getLevel().getSpawnLocation());
                 player.sendMessage(this.translate(player, TranslationKey.TELEPORT_SUCCESS, plotManager.getLevel().getFolderName()));
             }
-        });
+        }));
+
+        player.showFormWindow(window);
         return false;
     }
 
